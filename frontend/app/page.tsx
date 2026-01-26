d4df4c18f87104fdb90deb07f9032cdbee90da2b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2026-01-26 23:35:25 +0000

    color

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
         <h1 className="text-light font-bold text-center text-4xl mt-8">
           Student Showcase 2026
         </h1>
-        <div className="mt-32 mb-8 px-6 py-4 rounded-xl bg-secondary text-dark shadow-lg">
+        <div className="mt-32 mb-8 px-6 py-4 rounded-xl bg-support text-dark shadow-lg">
           <p className="font-semibold text-center">
             🚧 This page is currently under construction. Check back soon!
           </p>
