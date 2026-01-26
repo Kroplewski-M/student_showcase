80ebd359f82c4a02f7c534d5038e7632a6ab6009
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2026-01-26 16:36:11 +0000

    added rust packages

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,15 @@
 use actix_web::cookie::Cookie;
 use actix_web::cookie::time::Duration;
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get};
+use dotenv::dotenv;
 
 #[get("/health")]
 async fn health() -> impl Responder {
+    let is_prod = std::env::var("RUST_ENV").unwrap_or_default() == "production";
     let cookie = Cookie::build("health_test", "alive")
         .path("/")
         .http_only(true)
-        // .secure(true) // enable in prod HTTPS
+        .secure(is_prod) // enable in prod HTTPS
         .same_site(actix_web::cookie::SameSite::Lax)
         .max_age(Duration::days(1))
         .finish();
@@ -23,11 +25,23 @@ async fn health_check(req: HttpRequest) -> impl Responder {
     "false"
 }
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    unsafe {
+        openssl_probe::try_init_openssl_env_vars();
+    }
+    if std::env::var_os("RUST_LOG").is_none() {
+        unsafe {
+            std::env::set_var("RUST_LOG", "actix_web=info");
+        }
+    }
     println!("API starting on 0.0.0.0:8080");
 
     HttpServer::new(|| App::new().service(health).service(health_check))
         .bind(("0.0.0.0", 8080))?
         .run()
-        .await
+        .await?;
+
+    Ok(())
 }
