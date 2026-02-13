6ab4c25216cf8f301b2df806f75560a563a61096
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2026-02-13 20:23:47 +0000

    added register page

---
+++ frontend/app/SVGS/CheckIcon.tsx
@@ -0,0 +1,18 @@
+export default function CheckIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="text-secondary"
+    >
+      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+      <polyline points="22 4 12 14.01 9 11.01" />
+    </svg>
+  );
+}
