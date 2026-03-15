51513f6773089ca4a5fd834808097c759742e6ba
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2026-03-15 17:05:57 +0000

    displaying site info

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,17 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 
+export interface siteInfoDto {
+  studentCount: number | null;
+  projectCount: number | null;
+}
+
 export default async function Home() {
+  const res = await fetch(`${process.env.API_INTERNAL_URL}/ref/site_info`);
+  const data: siteInfoDto | null = await res.json();
   return (
     <div className="bg-primary text-light">
-      <Hero />
+      <Hero siteInfo={data} />
       <About />
     </div>
   );
